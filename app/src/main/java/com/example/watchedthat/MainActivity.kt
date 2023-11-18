657d6f653814a38fcac8351cd0e6dc47a7ba735c
Bruce <115784539+BruceZZSeventy-Seven@users.noreply.github.com>
2023-11-18 14:29:00 +0100

    App icon Added HomeScreen in progress...

--- app/src/main/java/com/example/watchedthat/MainActivity.kt
+++ app/src/main/java/com/example/watchedthat/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.watchedthat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,12 +12,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.watchedthat.ui.theme.WatchedThatTheme
+// New Imports
+import com.example.watchedthat.model.VisualMedia
+import androidx.compose.material3.Card
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.platform.LocalContext
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WatchedThatTheme {
+           WatchedThatTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -28,7 +42,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -37,6 +50,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -44,3 +58,53 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+
+/*-----------------------------Prove--------------------
+@Composable
+fun FilmList(){
+    Text(
+        text = "New Prova"
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun NewFunction(){
+    WatchedThatTheme {
+        FilmList()
+    }
+}
+
+@Composable
+fun VisualMediaList(visualmedia: VisualMedia, modifier: Modifier = Modifier){
+    Card(modifier = modifier){
+        Column {
+            Image(
+                painter = painterResource(R.drawable.testimage), //To change
+                contentDescription = "null",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+
+            )
+            Text(
+                text = LocalContext.current.getString(visualmedia.id),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+
+
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun VisualMediaListPreview() {
+    VisualMediaList(Visualmedia("Prova", R.drawable.testimage))
+}
+*/
--- app/src/main/java/com/example/watchedthat/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/watchedthat/ui/screens/HomeScreen.kt
@@ -1,21 +1,201 @@
 package com.example.watchedthat.ui.screens
 
+import android.graphics.Paint.Align
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.watchedthat.R
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 
+/*--------------------ORIGINAL-----------------
 @Composable
-fun HomeScreen(
+fun HomeScreenOLD(
     movieDiscoveryState: MovieDiscoveryState,
     retryAction: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Text(text = "Home Screen")
 }
-
+---------------------------------*/
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen(movieDiscoveryState = MovieDiscoveryState.Loading, retryAction = {})
+    DisplayImages()
+    GreetingText(message = "Go to Film", from ="Click" )
+    //HomeScreen(movieDiscoveryState = MovieDiscoveryState.Loading, retryAction = {})
 }
+
+
+//-------------
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    movieDiscoveryState: MovieDiscoveryState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "WatchedThat")
+                },
+
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.Green,
+                    titleContentColor = Color.Black,
+                ),
+            )
+        }, content = {
+            Column(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+                    .background(Color.White),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+               /* Text(
+                    text = "Content of the page",
+                    fontSize = 30.sp,
+                    color = Color.Black
+                )*/
+               // GreetingText(message = "Go to MovieList", from = "")
+                DisplayImages()
+            }
+        })
+
+}
+
+@Composable
+fun GreetingImage(){
+    Image(
+        painter = painterResource(R.drawable.testimage),
+        contentDescription = null,
+        modifier = Modifier
+            .padding(16.dp)
+            .height(200.dp)
+            .width(200.dp)
+    )
+}
+
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column ( verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxSize()
+
+    ){
+        GreetingImage()
+        Text(
+            text = message,
+            fontSize = 50.sp,
+            modifier = Modifier
+        )
+        Text(
+            text = from,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun DisplayImages(modifier: Modifier = Modifier){
+    val imageGit = painterResource(R.drawable.git_icon)
+    val imagePhone = painterResource(R.drawable.phone_icon)
+    val imageMail = painterResource(R.drawable.email_icon)
+
+    Image(
+        painter = painterResource(R.drawable.home_wallpaper),
+        contentDescription = null,
+        contentScale = ContentScale.FillBounds,
+        modifier = Modifier
+    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        GreetingIcon(imageGit, "PAMN Project")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.Start
+    ) {
+        GreetingIcon(imagePhone, "+00 1111222233")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.End
+    ) {
+        GreetingIcon(imageMail, "@ulpgc")
+    }
+}
+
+
+
+@Composable
+fun GreetingIcon(image: Painter, message: String){
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = Modifier
+            .padding(16.dp)
+            .height(60.dp)
+            .width(60.dp)
+    )
+    Text(
+        text = message,
+        fontSize = 15.sp,
+        modifier = Modifier
+    )
+}
+
